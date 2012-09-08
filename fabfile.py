ccc224e7a2107fb4340b5369f60c3e58ba4eaea7
Danny Jacob <danjac@danjac-ThinkPad-SL510.(none)>
2012-09-08 21:24:14 +0300

    fabfile added

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from fabric import api
+
+api.env.hosts=['178.79.185.15']
+
+def deploy():
+
+    with api.cd(api.env.deploy_path):
+
+        api.run("git pull")
+        api.run("%s/python setup.py develop" % api.env.python_path)
+        api.run("%s/python setup.py test" % api.env.python_path)
+        #api.run("%s/alembic upgrade head" % api.env.python_path)
+        api.sudo("supervisorctl restart photoapp")
+        
+
+
