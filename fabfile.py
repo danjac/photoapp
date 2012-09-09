e7465e511d3d60d31081ac5f06c75d2dfc89e427
Danny Jacob <danjac@danjac-ThinkPad-SL510.(none)>
2012-09-09 15:43:25 +0300

    Use requirements.txt in deployment

--- fabfile.py
+++ fabfile.py
@@ -7,7 +7,7 @@ def deploy():
     with api.cd(api.env.deploy_path):
 
         api.run("git pull")
-        api.run("%s/python setup.py develop" % api.env.python_path)
+        api.run("%s/pip install -r requirements.txt" % api.env.python_path)
         api.run("%s/python setup.py test" % api.env.python_path)
         #api.run("%s/alembic upgrade head" % api.env.python_path)
         api.sudo("supervisorctl restart photoapp")
