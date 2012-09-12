0d1cedbc4262162ea569e0f890d65f2128bbbf0d
Danny Jacob <danjac354@gmail.com>
2012-09-12 15:40:31 +0300

    Replaced static dirs

--- fabfile.py
+++ fabfile.py
@@ -12,10 +12,16 @@ def deploy():
 
     with api.cd(api.env.deploy_path):
 
+        # deploy update
         api.run("git pull")
-        #api.run("%s/pip install -r requirements.txt" % api.env.python_path)
         api.run("%s/python setup.py develop" % api.env.python_path)
+
+        # run tests: drop test DB to ensure clean run
         api.run("%s/drop_test_db" % api.env.python_path)
         api.run("%s/python setup.py test" % api.env.python_path)
+
+        # run migrations
         api.run("%s/alembic upgrade head" % api.env.python_path)
+
+        # restart long-running processes
         api.sudo("supervisorctl restart photoapp")
