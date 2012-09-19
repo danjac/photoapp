7f1a646e0ce18018f3fe3ef01ce19da2d4aab286
Danny Jacob <danjac354@gmail.com>
2012-09-19 11:34:37 +0300

    More docstrings

--- photoapp/mail.py
+++ photoapp/mail.py
@@ -7,10 +7,19 @@ from .manager import on_commit
 
 
 def get_mailer(request):
+    """Custom request property: adds IMailer instance to
+    current request.
+    """
+
     return request.registry.queryUtility(IMailer)
 
 
 def includeme(config):
+    """Pyramid configuration hook:
+
+    Registers IMailer instance and adds to current request
+    as `mailer` property.
+    """
 
     config.registry.registerUtility(
         mailer_settings_factory(config.get_settings()), IMailer)
@@ -19,6 +28,26 @@ def includeme(config):
 
 
 def mailer_settings_factory(settings, prefix='photoapp.mailer.'):
+    """Returns the correctly configured specific implementation
+    of an IMailer object based on settings.
+
+    The specific implementation is determined by the `type`
+    setting, currently:
+
+    smtp:
+        send mail from SMTP server
+    console:
+        print mail messages to stdout (does not send any mail)
+
+    `console` is the default type.
+
+    Args:
+        settings: dict of settings
+        prefix: specific prefix to find mailer-specific settings
+
+    Returns:
+        IMailer instance
+    """
 
     mailer_type = settings.get(prefix + 'type', 'console')
     mailer_cls = _mailer_classes[mailer_type]
@@ -26,16 +55,40 @@ def mailer_settings_factory(settings, prefix='photoapp.mailer.'):
 
 
 class IMailer(Interface):
+    """Abstracts mail sending functionality.
+    """
 
     def from_settings(settings, prefix):
-        pass
+        """Create instance of IMailer object from settings
+
+        Args:
+            settings: dict of settings
+            prefix: specific prefix to find mailer-specific settings
+
+        Returns:
+            IMailer instance.
+        """
 
     def send(msg):
-        pass
+        """Sends a mail message according underlying implementation.
+
+        Args:
+            msg: mailer.Message instance
+        """
 
 
 @implementer(IMailer)
 class SMTP_Mailer(mailer.Mailer):
+    """Sends email message through SMTP. The following specific settings are
+    required:
+
+        host: host (default `localhost`)
+        port: port number (default 0)
+        use_tls: TLS-enabled (default False)
+        usr: username
+        pwd: password
+        from_address: default "from" address used if none provided
+    """
 
     @classmethod
     def from_settings(cls, settings, prefix):
@@ -70,8 +123,7 @@ class SMTP_Mailer(mailer.Mailer):
 
 @implementer(IMailer)
 class ConsoleMailer(object):
-    """
-    Just dumps message to stdout. Use for development/testing.
+    """Just dumps message to stdout. Use for development/testing.
     """
 
     @classmethod
