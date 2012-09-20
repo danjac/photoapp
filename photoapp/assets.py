517e1dcafe8338b19ce168d7584f4e014606fe04
Danny Jacob <danjac354@gmail.com>
2012-09-20 19:34:15 +0300

    fix for jquery

--- photoapp/assets.py
+++ photoapp/assets.py
@@ -17,7 +17,7 @@ bootstrap_js = Bundle(
 
 
 jquery_js = Bundle(
-    'jquery/jquery-1.8.0-min.js',
+    'jquery/jquery-1.8.0.min.js',
     'jquery/jqcloud-1.0.1.min.js',
     'jquery/underscore-min.js',
     filters='uglifyjs',
